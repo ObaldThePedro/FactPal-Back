25a9225e23bcd6ec4c091f95ccfbcd880980fc85
ObaldThePedro <pedro.nunes.loureiro@gmail.com>
2019-07-26 13:20:35 +0100

    Added JWT Authentication

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,17 +6,19 @@ class Api::UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-          render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
-    end
-     
-      private
-      def user_params
-        params.require(:user).permit(:email, :password)
+      user = User.create( user_params )
+      if user.valid?
+          render json: { user: UserSerializer.new(user), token: issue_token(user_id: user.id) }, status: :created
+      else
+          render json: { errors: user.errors.full_messages }, status: :not_accepted
       end
+  end
+    
+    private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+
+    skip_before_action :authorize, only: [:create]
     
 end
